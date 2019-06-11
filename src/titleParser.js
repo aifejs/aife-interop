44b7eb7b9b7ff467e089734377bf596e333729f0
Konstantin Kitmanov <konstantin.kitmanov@fabres.pl>
2019-06-11 16:25:14 +0200

    new passage header format parser and tests

---
+++ src/titleParser.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const MODE_TITLE = 'MODE_TITLE';
+const MODE_TAGS = 'MODE_TAGS';
+const MODE_META = 'MODE_META';
+
+const mustEscape = '[]{}\\'.split('');
+const escapeStart = '\\';
+
+function isEscaped(char) {
+    return mustEscape.includes(char);
+}
+
+function titleParser(stream) {
+    let mode = MODE_TITLE;
+    let pos = 0;
+
+    let title = '';
+    let tags = '';
+    let meta = '';
+
+    while (pos < stream.length) {
+        const char = stream[pos];
+
+        if (mode === MODE_TITLE) {
+            if (char === escapeStart) {
+                if (isEscaped(stream[pos + 1])) {
+                    title += stream[pos + 1];
+
+                    pos++;
+                } else {
+                    title += char;
+                }
+                pos++;
+            } else if (char === '[') {
+                mode = MODE_TAGS;
+            } else if (char === '{') {
+                mode = MODE_META;
+            } else {
+                title += char;
+                pos++;
+            }
+        } else if (mode === MODE_TAGS) {
+            if (char === escapeStart) {
+                if (isEscaped(stream[pos + 1])) {
+                    tags += stream[pos + 1];
+                    pos++;
+                } else {
+                    tags += char;
+                }
+                pos++;
+            } else if (char === '{') {
+                mode = MODE_META;
+            } else {
+                tags += char;
+                pos++;
+            }
+        } else if (mode === MODE_META) {
+            meta += char;
+            pos++;
+        }
+    }
+
+    return {
+        title: title.trim(),
+        tags: tags.trim(),
+        meta: meta.trim(),
+    };
+}
+
+module.exports = {
+    titleParser,
+};
---
+++ test/titleParser.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const test = require('ava');
+const {titleParser,} = require('../src/titleParser.js');
+
+const simpleFixture = ':: An overgrown path';
+const tagsFixture = ':: An overgrown path [forest spooky]';
+const metaFixture = ':: An overgrown path {"position":"600,400","size":"100,200"}';
+const fullFixture = ':: An overgrown path [forest spooky] {"position":"600,400","size":"100,200"}';
+const escapedFixture = ':: A\\n \\[over\\{grown\\} path\\] [fo\\re[st\\] s\\{pook\\}y] {"position":"600,400","size":"100,200"}'; // eslint-disable-line max-len
+
+test('titleParser: simple', (t) => {
+    const simpleTitle = titleParser(simpleFixture);
+    t.is(simpleTitle.meta, '');
+    t.deepEqual(simpleTitle.tags, '');
+    t.is(simpleTitle.title, ':: An overgrown path');
+});
+
+test('titleParser: with tags', (t) => {
+    const taggedTitle = titleParser(tagsFixture);
+    t.is(taggedTitle.meta, '');
+    t.deepEqual(taggedTitle.tags, '[forest spooky]');
+    t.is(taggedTitle.title, ':: An overgrown path');
+});
+
+test('titleParser: with meta', (t) => {
+    const metaTitle = titleParser(metaFixture);
+    t.is(metaTitle.meta, '{"position":"600,400","size":"100,200"}');
+    t.deepEqual(metaTitle.tags, '');
+    t.is(metaTitle.title, ':: An overgrown path');
+});
+
+test('titleParser: with meta and tags', (t) => {
+    const fullTitle = titleParser(fullFixture);
+    t.is(fullTitle.meta, '{"position":"600,400","size":"100,200"}');
+    t.deepEqual(fullTitle.tags, '[forest spooky]');
+    t.is(fullTitle.title, ':: An overgrown path');
+});
+
+test('titleParser: with escape sequences', (t) => {
+    const fullTitle = titleParser(escapedFixture);
+    t.is(fullTitle.meta, '{"position":"600,400","size":"100,200"}');
+    t.deepEqual(fullTitle.tags, '[fo\\re[st] s{pook}y]');
+    t.is(fullTitle.title, ':: A\\n [over{grown} path]');
+});
